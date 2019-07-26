c9412dc841ad029e8009d28608dcd50fa4662992
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-07-26 15:59:47 +0300

    Execute command

--- interavtive-shell/__init__.py
+++ interavtive-shell/__init__.py
@@ -2,7 +2,7 @@ from ansimarkup import ansiprint as print
 
 from sarasvati.plugins import ApplicationPlugin
 from sarasvati.commands.result import ErrorCommandLineResult, MessageCommandLineResult
-
+from sarasvati.commands.command import Command
 
 class InteractiveShellApplicationPlugin(ApplicationPlugin):
     def __init__(self):
@@ -11,13 +11,16 @@ class InteractiveShellApplicationPlugin(ApplicationPlugin):
     def activate(self):
         super().activate()
         brain = self._api.brain.open("default.json")
-
         plugin = self._api.plugins.get(category="CommandLine")
 
         prompt = ""
         while prompt != "/q":
             prompt = input()
             result = plugin.execute(prompt)
+            
+            if isinstance(result, Command):
+                result.do()
+            
             if isinstance(result, MessageCommandLineResult):
                 if isinstance(result.message, str):
                     print(result.message)
