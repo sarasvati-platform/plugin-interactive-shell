336ab90b469e2d5cdd8d49a48d80b02a7ef17ae2
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-07-26 19:08:26 +0300

    execute command if returned

--- interavtive-shell/__init__.py
+++ interavtive-shell/__init__.py
@@ -2,7 +2,7 @@ from ansimarkup import ansiprint as print
 
 from sarasvati.plugins import ApplicationPlugin
 from sarasvati.commands.result import ErrorCommandLineResult, MessageCommandLineResult
-from sarasvati.commands.command import Command
+from sarasvati.commands.command import Command, CommandException
 
 class InteractiveShellApplicationPlugin(ApplicationPlugin):
     def __init__(self):
@@ -16,10 +16,17 @@ class InteractiveShellApplicationPlugin(ApplicationPlugin):
         prompt = ""
         while prompt != "/q":
             prompt = input()
-            result = plugin.execute(prompt)
+            result = None
             
+            try:
+                result = plugin.execute(prompt)
+            except CommandException as ex:
+                print(f"<red>{ex.message}</red>")
+    
             if isinstance(result, Command):
-                result.do()
+                result = result.do()
+                if result.message:
+                    print(result.message)
             
             if isinstance(result, MessageCommandLineResult):
                 if isinstance(result.message, str):
@@ -27,6 +34,6 @@ class InteractiveShellApplicationPlugin(ApplicationPlugin):
                 elif isinstance(result.message, list):
                     for line in result.message:
                         print(line)
-
+            
     def deactivate(self):
         pass
