0506d38884bcda7a66930cdb2fee52348ed6ccc9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-08-07 11:30:23 +0300

    syntax highlight, autocomplete

--- app.py
+++ app.py
@@ -1,37 +1,57 @@
 from ansimarkup import ansiprint as print
+from prompt_toolkit import PromptSession
+from prompt_toolkit.lexers import PygmentsLexer
 
 from sarasvati.commands.command import Command, CommandException, CommandResult
-from sarasvati.commands.result import (ErrorCommandLineResult,
-                                       MessageCommandLineResult)
+
+from .prompt import PromptCompleter, PromptLexer
 
 
 class InteractiveShellApplication:
     def __init__(self, api):
         self.__api = api
+        
+        # initialize command line
+        self.__command_line = self.__api.plugins.get(category="CommandLine")
+        self.__prompt_session = PromptSession(
+            lexer=PygmentsLexer(PromptLexer),
+            completer=PromptCompleter(
+                self.__command_line.get_commands(), api))
 
     def run(self):
-        path = self.__api.config.stores.local.path
-        brain = self.__api.brains.open("local://" + path + "/default")
-        plugin = self.__api.plugins.get(category="CommandLine")
-
-        prompt = ""
-        while prompt != "/q":
-            brain = self.__api.brains.active
-            active_thought = brain.active_thought
-            prompt = input(brain.name + "@" + (active_thought.title + "> " if active_thought else "> "))
-            result = None
-            
+        self.__open_brain()
+        
+        command = ""
+        while command != "/q":
             try:
-                result = plugin.execute(prompt)
+                command = self.__prompt()
+                self.__execute(command)
             except CommandException as ex:
                 print(f"<red>{ex.message}</red>")
 
-            if isinstance(result, Command):
-                result = result.do()
+    def __open_brain(self, path:str = "default"):
+        path = self.__api.config.stores.local.path  # todo: config may not exist
+        return self.__api.brains.open("local://" + path + "/default")
 
-            if isinstance(result, (MessageCommandLineResult, CommandResult)):
-                if isinstance(result.message, str):
-                    print(result.message)
-                elif isinstance(result.message, list):
-                    for line in result.message:
-                        print(line)
+    def __prompt(self):
+        brain = self.__api.brains.active
+        thought_title = brain.active_thought.title if brain.active_thought else ""
+        prompt_string = brain.name +  ("@" + thought_title + "> " if thought_title else "> ")
+        return self.__prompt_session.prompt(prompt_string)
+
+    def __execute(self, command):
+        result = None
+        try:
+            result = self.__command_line.execute(command)
+        except CommandException as ex:
+            print(f"<red>{ex.message}</red>")
+
+        if isinstance(result, Command):
+            result = result.do()
+
+        if isinstance(result, (CommandResult)):
+            if isinstance(result.message, str):
+                print(result.message)
+            elif isinstance(result.message, list):
+                for line in result.message:
+                    print(line)
---
+++ prompt.py
@@ -0,0 +1,54 @@
+from prompt_toolkit.completion import Completer, Completion
+from prompt_toolkit.document import Document
+from pygments.lexer import RegexLexer
+from pygments.token import Keyword, Operator
+
+
+class PromptLexer(RegexLexer):
+    tokens = {
+        'root': [
+            (r'/(\w|-)*', Keyword),
+            (r'(\w*):', Operator)
+        ]
+    }
+
+class PromptCompleter(Completer):
+    def __init__(self, commands, api):
+        self.__commands = commands
+        self.__mode = ""
+        self.__start = 0
+        self.__api = api
+
+    def get_completions(self, document, complete_event):
+        self.__switch_state(document)
+
+        if self.__mode is "command":
+            commands = {c: i for c, i in self.__commands.items() if document.text in c}
+            for command, info in commands.items():
+                yield Completion(command, 
+                    start_position=-len(document.text), 
+                    display_meta=info.description)
+
+        if self.__mode is "arg":
+            text = document.text[self.__start:]
+            if len(text) >= 1:
+                thoughts = self.__api.brains.active.find_thoughts({
+                    "field": "definition.title",
+                    "operator": "~~",
+                    "value": text
+                })
+                for thought in thoughts:
+                    yield Completion(thought.title, start_position=-len(text))
+
+    def __switch_state(self, document: Document):
+        if document.char_before_cursor is "/":
+            self.__mode = "command"
+            self.__start = document.cursor_position
+
+        if document.char_before_cursor is ":":
+            self.__mode = "arg"
+            self.__start = document.cursor_position
+
+        if "/" in document.text and document.char_before_cursor is " ":
+            self.__mode = "arg"
+            self.__start = document.cursor_position
