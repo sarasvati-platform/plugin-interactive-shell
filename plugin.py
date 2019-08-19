5f181be7aee0b9de37145d386c232fc483bcc6da
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-08-19 19:02:39 +0300

    check dependencies

--- plugin.py
+++ plugin.py
@@ -7,6 +7,14 @@ class InteractiveShellApplicationPlugin(ApplicationPlugin):
     def __init__(self):
         super().__init__()
 
+    def check_dependencies(self, api):
+        command_lines = api.plugins.find(category="CommandLine")
+        if not command_lines:
+            raise Exception("Sarasvati Interactive Shell requires 'CommandLine' plugin.")
+        if len(command_lines) > 1:
+            raise Exception("Too many 'CommandLine' plugins registered.")
+
+
     def activate(self):
         super().activate()
         app = InteractiveShellApplication(self._api)
